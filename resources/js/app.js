1bc1bf2bdb151d87b6256f6fa92c8e3dc3b14f9a
Thales Bento <dev.tbarbosa.bento@gmail.com>
2023-02-01 18:42:46 -0300

    Implements router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,12 @@
 import './bootstrap';
 import { createApp } from 'vue';
 import App from './src/App.vue';
+import Roteador from './src/routers/Roteador.vue';
 
 import '@fortawesome/fontawesome-free/css/all.css'
 import 'bulma/css/bulma.min.css'
 
 const app = createApp();
 app.component('app', App);
-app.mount('#app');
+app.use(Roteador)
+   .mount('#app');
