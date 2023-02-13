4c01b82cadb5f624820f8b1e37b530501d8cfae6
Thales Bento <dev.tbarbosa.bento@gmail.com>
2023-02-13 15:04:11 -0300

    Add pinia

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue';
 import App from '@/App.vue';
 import Roteador from '@/routers/Roteador.vue';
 import { key, store } from '@/store';
+import { createPinia } from 'pinia';
 
 import '@fortawesome/fontawesome-free/css/all.css'
 import 'bulma/css/bulma.min.css'
@@ -10,4 +11,5 @@ import 'bulma/css/bulma.min.css'
 createApp(App)
     .use(Roteador)
     .use(store, key)
+    .use(createPinia())
     .mount('#app');
